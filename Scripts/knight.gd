33ba584becce4bec73f9a41621e8946232503373
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-08-09 17:58:50 -0500

    added death function into script.

--- Scripts/knight.gd
+++ Scripts/knight.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
-
+extends KinematicBody2D
+extends StaticBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -450.0
-
+var is_dying = false
+var ascent_distance = 0
+export var ascent_speed = 300
+export var rotation_speed = 180
+export var descent_speed = 300
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -25,3 +30,24 @@ func _physics_process(delta):
 # Death function and animation
 # func _on_hitbox_body_entered(body:Node2D):
 # 	queue_free()
+
+	var collision = move_and_collide(velocity)
+	if collision:
+		if collision.collider.is_in_group("obstacle"):
+			player_death()
+	
+	if is_dying:
+        if ascent_distance < 300:
+            velocity.y = -ascent_speed
+            rotation += rotation_speed * delta
+            ascent_distance += ascent_speed * delta
+        else:
+            velocity.y = descent_speed
+            if position.y > get_viewport_rect().size.y / 2:
+                # Handle character death completion, e.g., queue_free()
+                queue_free()
+    else:
+		#this is for normal player movement.		
+
+func player_death():
+	is_dying = true
