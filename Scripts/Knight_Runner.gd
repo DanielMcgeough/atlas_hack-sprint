bb97c7ce8a98c7a1e7f48a435c116af6f11ac630
lrwyatt801 <loganwyatt1995@gmail.com>
2024-08-10 20:06:17 -0500

    Working on gameover

--- Scripts/Knight_Runner.gd
+++ Scripts/Knight_Runner.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 var speed = 200
 var is_dead = false
+var gameover = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,7 +15,8 @@ func _physics_process(delta):
 
 func _process(_delta):
 	var viewport_rect = get_viewport_rect()
-	if $Knight.position.y > viewport_rect.end.y and is_dead:
+	if $Knight.position.y > viewport_rect.end.y and is_dead and !gameover:
+		gameover = true
 		print("game over")
 
 func _on_knight_is_dying_changed(new_value):
