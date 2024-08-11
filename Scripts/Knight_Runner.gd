dfffe262914f10c26b9e44e045c1e2e9f60aab17
lrwyatt801 <loganwyatt1995@gmail.com>
2024-08-10 19:38:37 -0500

    Update for signals and complete death animation

--- Scripts/Knight_Runner.gd
+++ Scripts/Knight_Runner.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 var speed = 200
 
 # Called when the node enters the scene tree for the first time.
@@ -10,3 +11,8 @@ func _physics_process(delta):
 	for ground in $Ground.get_children():
 		ground.position.x -= speed*delta
 
+func _on_knight_is_dying_changed(new_value):
+	print("from game: ", new_value)
+
+func _on_knight_char_dead_off_screen():
+	print("game over")
--- Scripts/knight.gd
+++ Scripts/knight.gd
@@ -1,17 +1,24 @@
 extends CharacterBody2D
-extends KinematicBody2D
-extends StaticBody2D
+
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -450.0
 var is_dying = false
+var change_is_dying = false
+signal is_dying_changed(new_value)
+signal char_dead_off_screen()
 var ascent_distance = 0
-export var ascent_speed = 300
-export var rotation_speed = 180
-export var descent_speed = 300
+var ascent_speed = 300
+var rotation_speed = 180
+var descent_speed = 300
+
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+
+func _ready():
+	pass
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -22,32 +29,39 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	position.x = 123
+	position.x = 192
 
 	move_and_slide()
+		
 
 
-# Death function and animation
-# func _on_hitbox_body_entered(body:Node2D):
-# 	queue_free()
+func _on_hitbox_body_shape_entered(_body_rid, body, _body_shape_index, _local_shape_index):
+	var delta = get_physics_process_delta_time()
+	if body.name == "Body":
+		pass
+	else:
+		change_is_dying = true
+		if ascent_distance < 300:
+				velocity.y = -ascent_speed
+				rotation += rotation_speed * delta
+				ascent_distance += ascent_speed * delta
+		else:
+			velocity.y = descent_speed
+			if position.y > get_viewport_rect().size.y / 2:
+				# Handle character death completion, e.g., queue_free()
+				queue_free()
+				print("Char off scree")
+				emit_signal("char_dead_off_screen")
 
-	var collision = move_and_collide(velocity)
-	if collision:
-		if collision.collider.is_in_group("obstacle"):
-			player_death()
-	
-	if is_dying:
-        if ascent_distance < 300:
-            velocity.y = -ascent_speed
-            rotation += rotation_speed * delta
-            ascent_distance += ascent_speed * delta
-        else:
-            velocity.y = descent_speed
-            if position.y > get_viewport_rect().size.y / 2:
-                # Handle character death completion, e.g., queue_free()
-                queue_free()
-    else:
-		#this is for normal player movement.		
+func check_dying_condition():
+	return change_is_dying
 
-func player_death():
-	is_dying = true
+func _process(_delta):
+	change_is_dying = check_dying_condition()
+	if change_is_dying != is_dying:
+		is_dying = change_is_dying
+		print("sending signal")
+		emit_signal("is_dying_changed", is_dying)
+		if is_dying:
+			$Body.set_deferred("disabled", true)
+			$"Hitbox/Hitbox Area".set_deferred("disabled", true)
