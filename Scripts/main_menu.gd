b4940009ff559e8285847a73cd0e491e9f5e687a
lrwyatt801 <loganwyatt1995@gmail.com>
2024-08-12 21:01:12 -0500

    Add background

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -12,7 +12,7 @@ func _process(_delta):
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/Knight_Runner.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Knight_Runner.tscn")
 
 
 func _on_quit_button_pressed():
